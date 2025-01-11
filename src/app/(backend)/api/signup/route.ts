b410bf23e74976845d25c4ac599dba93e70be72a
Filip Nystrand <filip.nystrand@gmail.com>
2025-01-11 23:17:37 +0100

    Payload rätt najs ändå

---
+++ src/app/(backend)/api/signup/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from 'next/server'
+import { getPayload } from 'payload'
+import config from '@payload-config'
+
+export const GET = async () => {
+  const payload = await getPayload({ config })
+
+  const event = await payload.create({
+    collection: 'events',
+    data: {
+      name: 'My first event',
+      domain: 'snyggdoman.se',
+      hero: {
+        eventDate: new Date().toISOString(),
+        media: 2,
+      },
+      layout: [
+        {
+          blockType: 'mediaBlock',
+          blockName: 'Media Block',
+          media: 2,
+        },
+      ],
+    },
+  })
+
+  await payload.create({
+    collection: 'users',
+    data: {
+      name: 'Filip Nystrand',
+      email: 'filip.nystrand+api@gmail.com',
+      password: 'testtest123',
+      role: 'mediumUser',
+      events: [event.id],
+    },
+  })
+
+  return NextResponse.json({
+    status: 'success',
+    message: 'Hello from the API!',
+    timestamp: new Date().toISOString(),
+  })
+}
