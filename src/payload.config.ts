9ca90b5826126bffc53c6fadecd928de4c6d9fe4
Filip Nystrand <filip.nystrand@gmail.com>
2025-03-09 18:45:45 +0100

    Upload from client

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -80,6 +80,7 @@ export default buildConfig({
       },
       // Token provided by Vercel once Blob storage is added to your Vercel project
       token: process.env.BLOB_READ_WRITE_TOKEN,
+      clientUploads: true,
     }),
     // storage-adapter-placeholder
   ],
